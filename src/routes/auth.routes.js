a990baf949d0f0479564c91a7194689a5def6719
estefa99 <estefanobugari@gmail.com>
2023-11-27 18:38:22 -0300

    Agregamos el middleware validateToken para proteger las rutas e inicializamos la ruta de products

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import {register, login, logout} from "../controllers/auth.controller.js";
+import {register, login, logout, crud} from "../controllers/auth.controller.js";
+import { authRequired } from "../middleware/validateToken.js";
 
 const router = Router();
 
@@ -8,5 +9,7 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.get("/crud", authRequired, crud); //Tiene un middleware, primero se ejecuta la funcion authRequired y luego la otra
 
 export default router;
