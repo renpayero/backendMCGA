e59dda3dd324e4a95c1124f0ddff2d74fe663eec
Renzo Payero <renzopayero@hotmail.com>
2023-12-13 23:40:35 -0300

    sameSite establecido en False

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -68,7 +68,7 @@ export const login = async(req: Request, res: Response) => {
 
         const token: string | undefined = await createAccessToken({ id: userFound._id }); //Creamos el token para mandarlo al front
         
-        res.cookie("token", token); // guarda el token en una cookie, y lo enviamos en el header seteado como cookie.
+        res.cookie("token", token, {sameSite:false,}); // guarda el token en una cookie, y lo enviamos en el header seteado como cookie.
         return res.json({
             id: userFound._id,
             username: userFound.username,
