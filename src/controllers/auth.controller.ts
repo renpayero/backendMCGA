fbf3d02090c080e4622fe682b31ff293e2596b73
Renzo Payero <renzopayero@hotmail.com>
2023-12-17 21:09:32 -0300

    set cookie

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -74,7 +74,7 @@ export const login = async (req: Request, res: Response) => {
     // res.cookie("token", token, { httpOnly: true, secure: true, sameSite: 'none' as const,}); // guarda el token en una cookie, y lo enviamos en el header seteado como cookie.
     
     res.writeHead(200, { 
-      "Set-Cookie": ["q=city;HttpOnly", "lat=45; SameSite=Lax", "another=foo;SameSite=Lax;"] 
+      "Set-Cookie": `token=${token}; HttpOnly; Secure; SameSite=None` 
     });
     
     return res.json({
