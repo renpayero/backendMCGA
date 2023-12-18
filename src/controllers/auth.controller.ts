31b3a29763c15e4b30ce630ff91d40c9028fb4d6
Renzo Payero <renzopayero@hotmail.com>
2023-12-17 21:07:44 -0300

    writehead en la res

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -71,7 +71,12 @@ export const login = async (req: Request, res: Response) => {
       id: userFound._id,
     }); //Creamos el token para mandarlo al front
 
-    res.cookie("token", token, { httpOnly: true, secure: true, sameSite: 'none' as const,}); // guarda el token en una cookie, y lo enviamos en el header seteado como cookie.
+    // res.cookie("token", token, { httpOnly: true, secure: true, sameSite: 'none' as const,}); // guarda el token en una cookie, y lo enviamos en el header seteado como cookie.
+    
+    res.writeHead(200, { 
+      "Set-Cookie": ["q=city;HttpOnly", "lat=45; SameSite=Lax", "another=foo;SameSite=Lax;"] 
+    });
+    
     return res.json({
       id: userFound._id,
       username: userFound.username,
